b22604e5e381cd2c9177fdf12af4237b0e16c710
Claudio Bertani <c.bertani@abletech.it>
2024-09-30 11:50:06 +0200

    Test TextboxWidget Invisible on dinamic creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,7 +24,7 @@ export class AppComponent implements AfterViewInit {
 
     WebViewer({
       path: '../lib',
-      initialDoc: '../files/webviewer-demo-annotated.pdf',
+      initialDoc: '../files/sample.pdf',
       licenseKey: 'your_license_key'  // sign up to get a free trial key at https://dev.apryse.com
     }, this.viewer.nativeElement).then(instance => {
       this.wvInstance = instance;
@@ -39,20 +39,62 @@ export class AppComponent implements AfterViewInit {
         console.log('annotations loaded');
       });
 
-      documentViewer.addEventListener('documentLoaded', () => {
+      documentViewer.addEventListener('documentLoaded', async () => {
         this.documentLoaded$.next();
-        const rectangleAnnot = new Annotations.RectangleAnnotation({
-          PageNumber: 1,
-          // values are in page coordinates with (0, 0) in the top left
-          X: 100,
-          Y: 150,
-          Width: 200,
-          Height: 50,
-          Author: annotationManager.getCurrentUser()
+       
+
+        const flags = new Annotations.WidgetFlags();
+        flags.set(Annotations.WidgetFlags.MULTILINE, false);
+        flags.set(Annotations.WidgetFlags.REQUIRED, false);
+
+        const field = new Annotations.Forms.Field("textBoxField", {
+          type: 'Tx',
+          flags: flags,
         });
-        annotationManager.addAnnotation(rectangleAnnot);
-        annotationManager.redrawAnnotation(rectangleAnnot);
+
+        const textBoxWidget = new Annotations.TextWidgetAnnotation(field, {});// {transparentBackground: "false"}
+        // textBoxWidget.addCustomAppearance(this.getDocument(), {transparentBackground: "false"});
+        textBoxWidget.setHeight(50);
+        textBoxWidget.setWidth(200);
+        textBoxWidget.setX(100);
+        textBoxWidget.setY(150);
+        textBoxWidget.setPageNumber(1);
+        annotationManager.getFieldManager().addField(field);
+        annotationManager.addAnnotation(textBoxWidget, {imported: true});
+        await annotationManager.drawAnnotationsFromList([textBoxWidget]);
+
+
       });
+
     })
+
+  }
+
+  async createTextbox(){
+    if(!this.wvInstance){
+      console.error("wvInstance undefined")
+      return;
+    }
+    const { documentViewer, Annotations, annotationManager } = this.wvInstance.Core;
+
+    const flags = new Annotations.WidgetFlags();
+    flags.set(Annotations.WidgetFlags.MULTILINE, false);
+    flags.set(Annotations.WidgetFlags.REQUIRED, false);
+
+    const field = new Annotations.Forms.Field("textBoxField", {
+      type: 'Tx',
+      flags: flags,
+    });
+
+    const textBoxWidget = new Annotations.TextWidgetAnnotation(field, {});// {transparentBackground: "false"}
+    // textBoxWidget.addCustomAppearance(this.getDocument(), {transparentBackground: "false"});
+    textBoxWidget.setHeight(50);
+    textBoxWidget.setWidth(200);
+    textBoxWidget.setX(300);
+    textBoxWidget.setY(300);
+    textBoxWidget.setPageNumber(1);
+    annotationManager.getFieldManager().addField(field);
+    annotationManager.addAnnotation(textBoxWidget, {imported: true});
+    await annotationManager.drawAnnotationsFromList([textBoxWidget]);
   }
 }
